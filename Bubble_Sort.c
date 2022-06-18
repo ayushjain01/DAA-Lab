ea0cf58d5944b849e80bc1a50c3f6894aecd2a05
Ayush Jain <ayushganna67@gmail.com>
2022-06-18 10:51:23 +0530

    Create Bubble_Sort.c

---
+++ Bubble_Sort.c
@@ -0,0 +1,56 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+void bubble(int a[], int n)
+{
+    int i, j, temp;
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[i])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
+
+void print_array(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t", arr[i]);
+        printf("\n");
+    }
+}
+void main()
+{
+    double total_time;
+    clock_t start, end;
+    int i, j, min, n, temp = 0;
+    printf("Enter the number of elements in the array\n");
+    scanf("%d", &n);
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        int r;
+        r = rand();
+        arr[i] = r;
+    }
+    printf("Before sorting\n");
+    print_array(arr, n);
+    start = clock();
+    bubble(arr, n);
+
+    end = clock();
+    total_time = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("After sorting\n");
+    print_array(arr, n);
+    printf("\nTotal time taken is %lf", total_time);
+}
